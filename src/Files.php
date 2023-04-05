222dbed72933341156bf3362e9e00dba3385c40a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-04-05 23:03:03 +0200

    Improved directory scanning

--- src/Files.php
+++ src/Files.php
@@ -1,28 +1,62 @@
 <?php
 
+declare(strict_types=1);
+
 namespace WyriHaximus\SubSplitTools;
 
+use FilesystemIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use Safe\Exceptions\FilesystemException;
+
+use function dirname;
+use function is_file;
+use function is_string;
+use function rtrim;
+use function Safe\file_get_contents;
+use function Safe\file_put_contents;
+use function Safe\mkdir;
+use function strlen;
+use function substr;
 use function WyriHaximus\Twig\render;
+
+use const DIRECTORY_SEPARATOR;
 
 final class Files
 {
+    /**
+     * @param array<mixed> $templateVariables
+     */
     public static function setUp(string $templates, string $destination, array $templateVariables): void
     {
-        foreach ([
-            ...glob(rtrim($templates, '/') . '/**', GLOB_NOSORT),
-            ...glob(rtrim($templates, '/') . '/**/*', GLOB_NOSORT),
-        ] as $fileName) {
-            echo $fileName, PHP_EOL;
-
-            if (!is_file($fileName)) {
+        foreach (
+            new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator(
+                    rtrim($templates, '/'),
+                    FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_PATHNAME,
+                ),
+            ) as $fileName
+        ) {
+            if (! is_string($fileName)) {
                 continue;
             }
+
+            if (! is_file($fileName)) {
+                continue;
+            }
+
             $newFilename = render(
                 $destination . DIRECTORY_SEPARATOR . substr($fileName, strlen($templates)),
                 $templateVariables,
             );
 
-            @mkdir(dirname($newFilename), 0744, true);
+            try {
+                /** @phpstan-ignore-next-line */
+                @mkdir(dirname($newFilename), 0744, true);
+            } catch (FilesystemException) {
+                // void @ignoreException
+            }
+
             file_put_contents(
                 $newFilename,
                 render(
